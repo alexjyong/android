89a4e8cfca2a7c7cc851e81ed5a50643d120ef54
Infi <infi@infi.sh>
2025-01-01 20:14:28 +0100

    feat(jbm): use jbm as RichMarkdown if experiment
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/components/markdown/RichMarkdown.kt
+++ app/src/main/java/chat/revolt/components/markdown/RichMarkdown.kt
@@ -3,11 +3,19 @@ package chat.revolt.components.markdown
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import chat.revolt.api.settings.Experiments
+import chat.revolt.markdown.jbm.JBM
+import chat.revolt.markdown.jbm.JBMRenderer
 import chat.revolt.ndk.Stendal
 
+@OptIn(JBM::class)
 @Composable
 fun RichMarkdown(input: String, modifier: Modifier = Modifier) {
     Column(modifier) {
-        MarkdownTree(node = Stendal.render(input))
+        if (Experiments.useKotlinBasedMarkdownRenderer.isEnabled) {
+            JBMRenderer(input)
+        } else {
+            MarkdownTree(node = Stendal.render(input))
+        }
     }
 }
