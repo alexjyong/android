79f4d97b75ef4552879f881d5a6a1f661bf84e54
Infi <infi@infi.sh>
2025-03-21 19:12:50 +0100

    chore: upgrade dependencies
    Signed-off-by: Infi <infi@infi.sh>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
     id("com.google.gms.google-services")
 }
 
-val composeBomVersion = "2024.10.01"
+val composeBomVersion = "2025.03.00"
 val accompanistVersion = "0.34.0"
 val okhttpVersion = "4.12.0"
 val navVersion = "2.8.7"
@@ -28,7 +28,7 @@ val glideVersion = "4.16.0"
 val ktorVersion = "3.0.0-beta-2"
 val media3Version = "1.5.0"
 val livekitVersion = "2.2.0"
-val material3Version = "1.4.0-alpha09"
+val material3Version = "1.4.0-alpha10"
 val androidXTestVersion = "1.6.1"
 
 fun property(fileName: String, propertyName: String, fallbackEnv: String? = null): String? {
