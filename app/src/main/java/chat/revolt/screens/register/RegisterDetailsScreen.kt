d33116d6f5fa4c1cbb0ddc35a70bd216021a28f9
Infi <infi@infi.sh>
2025-03-21 20:21:22 +0100

    feat: autofill in register screen
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/screens/register/RegisterDetailsScreen.kt
+++ app/src/main/java/chat/revolt/screens/register/RegisterDetailsScreen.kt
@@ -22,8 +22,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentType
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentType
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -172,7 +175,8 @@ fun RegisterDetailsScreen(
                     onChange = { viewModel.email = it },
                     label = stringResource(R.string.register_email),
                     type = KeyboardType.Email,
-                    action = ImeAction.Next
+                    action = ImeAction.Next,
+                    modifier = Modifier.semantics { contentType = ContentType.EmailAddress }
                 )
                 Text(
                     text = stringResource(R.string.register_email_verification_hint),
@@ -189,7 +193,9 @@ fun RegisterDetailsScreen(
                     value = viewModel.password,
                     onChange = { viewModel.password = it },
                     label = stringResource(R.string.register_password),
-                    type = KeyboardType.Password
+                    type = KeyboardType.Password,
+                    action = ImeAction.Done,
+                    modifier = Modifier.semantics { contentType = ContentType.NewPassword }
                 )
                 Text(
                     text = stringResource(R.string.register_password_rules),
