128daa39ff515e20b563128621f8db21096a2faf
Infi <infi@infi.sh>
2025-03-21 20:17:20 +0100

    feat: autofill in login screen
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/screens/login/LoginScreen.kt
+++ app/src/main/java/chat/revolt/screens/login/LoginScreen.kt
@@ -31,10 +31,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentType
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentType
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -225,7 +228,11 @@ fun LoginScreen(navController: NavController, viewModel: LoginViewModel = hiltVi
                     type = KeyboardType.Email,
                     action = ImeAction.Next,
                     onChange = viewModel::setEmail,
-                    modifier = Modifier.padding(vertical = 25.dp)
+                    modifier = Modifier
+                        .padding(vertical = 25.dp)
+                        .semantics {
+                            contentType = ContentType.EmailAddress
+                        }
                 )
                 SecureTextField(
                     passwordTextFieldState,
@@ -259,6 +266,9 @@ fun LoginScreen(navController: NavController, viewModel: LoginViewModel = hiltVi
                             }
                         }
                     },
+                    modifier = Modifier.semantics {
+                        contentType = ContentType.Password
+                    }
                 )
 
                 AnyLink(
